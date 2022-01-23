ca59e344ddb8bd5466d2a9575e7ca9890c9e28da
sergiikaplun <sikaplun@gmail.com>
2022-01-23 10:47:23 +0300

    Initial commit

---
+++ app/src/main/java/com/sikaplun/gb/kotlin/githubuseapi/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.sikaplun.gb.kotlin.githubuseapi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
